25e1dfde052c5954251e555334049d5e9f82cc31
Naveen Malik <nmalik@redhat.com>
2018-05-22 14:10:32 -0400

    Figuring out goclipse, added readme.

---
+++ README.md
@@ -0,0 +1,2 @@
+A very simple app that listens on port 8080.
+Any request query path is written to standard out and sent as a response to the client with a "Pong" prefix.
--- src/main/ping.go
+++ src/main/ping.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print(r.URL.Path)
 		fmt.Fprintf(w, "Pong, %q", html.EscapeString(r.URL.Path))
 	})
 	
