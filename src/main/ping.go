c49117d114bf2de91060d6abde433685f8190846
Naveen Malik <nmalik@redhat.com>
2019-02-05 13:57:41 -0500

    Added deployment from quay.io image

--- src/main/ping.go
+++ src/main/ping.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"log"
 	"fmt"
-    "net/http"
-    "html"
+	"html"
+	"log"
+	"net/http"
 
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-    http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print(r.URL.Path)
-        fmt.Fprintf(w, "Pong: %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Pong: %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
